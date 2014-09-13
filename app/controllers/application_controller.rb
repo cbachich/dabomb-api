d9bf264a14e1c4b2df4ef6fdcfb27f81efacaed5
Christopher Bachicha <cbachich@gmail.com>
2014-09-12 19:55:57 -0700

    Allow PUT for CORS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::API
 
   def cors_preflight_check
     if request.method == :options
-      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, DELETE'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, DELETE, PUT'
       headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Content-Type, Accept'
       headers['Access-Control-Max-Age'] = '1728000'
       render :text => '', :content_type => 'text/plain'
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::API
 
   def cors_set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, DELETE'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, DELETE, PUT'
     headers['Access-Control-Allow-Headers'] = 'Content-Type, Accept'
     headers['Access-Control-Max-Age'] = "1728000"
   end
